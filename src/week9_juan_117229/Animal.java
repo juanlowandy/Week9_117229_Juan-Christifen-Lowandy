a1673840d212136fb30f96cece9889b71959b2b6
Juan <juanlowandy@gmail.com>
2025-11-08 21:19:15 +0700

    Interface Animal

---
+++ src/week9_juan_117229/Animal.java
@@ -0,0 +1,6 @@
+package week9_juan_117229;
+
+public interface Animal {
+    public void eat();
+    public void travel();
+}
---
+++ src/week9_juan_117229/Mammal.java
@@ -0,0 +1,19 @@
+package week9_juan_117229;
+
+public class Mammal implements Animal {
+    public Mammal() {}
+
+    public void eat() {
+        System.out.println("Mammal eats");
+    }
+
+    public void travel() {
+        System.out.println("Mammal travel");
+    }
+
+    public static void main(String[] args) {
+        Mammal m = new Mammal();
+        m.eat();
+        m.travel();
+    }
+}
